5f403c5cd5ba7d76dde3e8756243e320dbc909a6
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-17 13:14:36 +0900

    Title: 두 정수에 대한 연산값, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240117/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -0,0 +1,13 @@
+def f(a,b):
+    if a > b:
+        a += 25
+        b *= 2
+    else:
+        b += 25
+        a *= 2
+    return a, b
+
+
+a,b = tuple(map(int,input().split()))
+a,b = f(a,b)
+print(a,b)
