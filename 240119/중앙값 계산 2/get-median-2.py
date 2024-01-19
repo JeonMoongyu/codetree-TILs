a4dd0b795106988e23e0cec7719b936d59555dde
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-19 13:34:47 +0900

    Title: 중앙값 계산 2, Time: 155ms, Memory: 24MB, Status: Passed - CodeTree

--- 240119/중앙값 계산 2/get-median-2.py
+++ 240119/중앙값 계산 2/get-median-2.py
@@ -2,4 +2,4 @@ n = int(input())
 arr = list(map(int,input().split()))
 for i in range(0,n,2):
     sorted_arr = sorted(arr[:i+1])
-    print(arr[i//2], end=" ")
+    print(sorted_arr[i//2], end=" ")
