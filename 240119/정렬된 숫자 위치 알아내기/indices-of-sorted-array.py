e54985eb16fcbb90e7cc7fc5a26223b2064dde1a
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-19 16:49:09 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 166ms, Memory: 25MB, Status: Passed - CodeTree

--- 240119/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
+++ 240119/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
@@ -1,20 +1,19 @@
 class Term:
-    def __init__(self, value=0, index=0, order=0):
+    def __init__(self, value=0, index=0):
         self.value = value
         self.index = index
-        self.order = order
 
 n = int(input())
 arr = list(map(int,input().split()))
 seq = [
-    Term(arr[i],i+1,0)
+    Term(arr[i],i+1)
     for i in range(n)
 ]
 
 seq.sort(key = lambda x: (x.value, x.index))
-for i in range(n):
-    seq[i].order = i+1
+ans = [0] * (n+1)
+for i, term in enumerate(seq,start=1):
+    ans[term.index] = i
 
-seq.sort(key = lambda x: x.index)
-for term in seq:
-    print(term.order, end=" ")
+for elem in ans[1:]:
+    print(elem, end=" ")
