5245962ca8ed42925db57f8868138f66d05f34e1
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-19 15:27:07 +0900

    Title: 비오는 날, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240119/비오는 날/rainy-day.py
+++ 240119/비오는 날/rainy-day.py
@@ -4,24 +4,12 @@ class Climate:
         self.day = day
         self.weather = weather
 
-
-def sooner(a,b):
-    a = tuple(map(int,a.split("-")))
-    b = tuple(map(int,b.split("-")))
-    return a<b
-
 n = int(input())
 
-data_rain = []
+ans = Climate("9999-99-99",0,0)
 for _ in range(n):
     date, day, weather = input().split()
-    if weather == "Rain":
-        data_rain.append(Climate(date,day,weather))
+    if (weather == "Rain") & (date < ans.date):
+        ans = Climate(date,day,weather)
 
-idx = 0
-for i in range(1,len(data_rain)):
-    if sooner(data_rain[i].date, data_rain[idx].date):
-        idx = i
-
-ans = data_rain[idx]
 print(ans.date, ans.day, ans.weather)
