0d0912c3a62d8bcc00fb9eee9b66e290f83083be
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-19 13:24:25 +0900

    Title: k번째로 신기한 문자열, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240119/k번째로 신기한 문자열/kth-special-string.py
+++ 240119/k번째로 신기한 문자열/kth-special-string.py
@@ -3,10 +3,9 @@ n, k = int(n), int(k)
 
 
 def is_start_t(word):
-    for i in range(len(t_str)):
-        if word[i] != t_str[i]:
-            return False
-    return True
+    if len(word) < len(t_str):
+        return False
+    return word[:len(t_str)]==t_str
 
 
 t_arr = []
