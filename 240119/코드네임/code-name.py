465f99b6e176f72bd53c58cdee50e1a7859c5bc1
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-19 14:38:08 +0900

    Title: 코드네임, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240119/코드네임/code-name.py
@@ -0,0 +1,16 @@
+class Agent:
+    def __init__(self, code_name=0, score=0):
+        self.code_name = code_name
+        self.score = score
+
+agents = []
+for _ in range(5):
+    code_name, score = input().split()
+    agents.append(Agent(code_name, int(score)))
+
+idx = 0
+for i in range(1,5):
+    if agents[i].score < agents[0].score:
+        idx = i
+
+print(agents[idx].code_name, agents[idx].score)
