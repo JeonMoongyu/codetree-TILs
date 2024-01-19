f34d77410f96c7c1fde9ba8a2ce197e0bd54d1f2
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-19 17:37:44 +0900

    Title: 그 요일은, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240119/그 요일은/the-day-of-the-day.py
+++ 240119/그 요일은/the-day-of-the-day.py
@@ -1,9 +1,18 @@
 days_month = [0,31,29,31,30,31,30,31,31,30,31,30,31]
+days_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
 def f(m,d):
     return sum(days_month[1:m])+d
 
 m1,d1,m2,d2 = tuple(map(int,input().split()))
 weekday = input()
+
+k = 0
+for i in range(7):
+    if weekday == days_week[i]:
+        k = i
+        break
+k = 7 if k == 0 else k
+
 diff = f(m2,d2) - f(m1,d1)
-print(diff//7)
+print((diff-k)//7+1)
