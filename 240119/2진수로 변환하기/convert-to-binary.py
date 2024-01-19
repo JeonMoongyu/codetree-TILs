d8d9f590ba67f5385b0ac2f8928e12076c9e5506
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-19 17:55:40 +0900

    Title: 2진수로 변환하기, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240119/2진수로 변환하기/convert-to-binary.py
+++ 240119/2진수로 변환하기/convert-to-binary.py
@@ -1,12 +1,12 @@
 n = int(input())
 digits = []
 
-if n==0:
-    digits = [0]
-else:
-    while n:
-        digits.append(n%2)
-        n //= 2
+while True:
+    if n<2:
+        digits.append(n)
+        break
+    digits.append(n%2)
+    n //= 2
 
 for digit in digits[::-1]:
     print(digit, end="")
