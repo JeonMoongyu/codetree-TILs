3c6af0fcc0712c1b20c5592b205b3359026cf7ac
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-22 01:13:28 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 204ms, Memory: 55MB, Status: Wrong Answer - CodeTree

---
+++ 240122/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -0,0 +1,41 @@
+plane = [
+    [0 for _ in range(2000)]
+    for _ in range(2000)
+]
+
+for k in range(1,-1,-1):
+    x1,y1,x2,y2 = tuple(map(int,input().split()))
+    for i in range(x1+1000,x2+1000):
+        for j in range(y1+1000,y2+1000):
+            plane[i][j] = k
+        
+
+a1,b1,a2,b2 = -1,-1,-1,-1
+
+def is_all_0(arr):
+    for elem in arr:
+        if elem != 0:
+            return False
+    return True
+
+for i in range(2000):
+    if is_all_0(plane[i]) == False:
+        a1 = i
+        break
+for i in range(a1+1,2000):
+    if is_all_0(plane[i]):
+        a2 = i
+        break
+
+for j in range(2000):
+    col_j = [ plane[i][j] for i in range(2000) ]
+    if is_all_0(col_j) == False:
+        b1 = j
+        break
+for j in range(b1+1,2000):
+    col_j = [ plane[i][j] for i in range(2000) ]
+    if is_all_0(col_j):
+        b2 = j
+        break
+
+print( (a2-a1)*(b2-b1) )
