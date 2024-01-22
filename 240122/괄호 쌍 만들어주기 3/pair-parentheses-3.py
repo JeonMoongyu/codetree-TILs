bd95f691ab10914a400ecf15129ff7753ca54583
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-22 21:17:31 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240122/괄호 쌍 만들어주기 3/pair-parentheses-3.py
+++ 240122/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -1,14 +1,9 @@
-def count_right(string):
-    cnt = 0
-    for elem in string:
-        if elem == ')':
-            cnt += 1
-    return cnt 
-
 string = input()
+leng = len(string)
 ans = 0
-for i in range(len(string)):
-    if string[i] == '(':
-        ans += count_right(string[i+1:])
+for i in range(leng):
+    for j in range(i+1,leng):
+        if string[i] == '(' and string[j] == ')':
+            ans += 1
 
 print(ans)
