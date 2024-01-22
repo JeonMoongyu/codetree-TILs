a48490d24a574c68710cabc1d2d0c9359ef3c4b1
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-22 22:27:23 +0900

    Title: Carry 피하기 2, Time: 108ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240122/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,18 @@
+def not_carry(a,b,c):
+    if a < 10 and b < 10 and c < 10:
+        return a+b+c < 10
+    return (a%10)+(b%10)+(c%10) < 10 and not_carry(a//10,b//10,c//10)
+
+
+n = int(input())
+arr = [
+    int(input())
+    for _ in range(n)
+]
+max_sum = -1
+for i in range(n-2):
+    for j in range(n-1):
+        for k in range(n):
+            if not_carry(arr[i],arr[j],arr[k]):
+                max_sum = max(max_sum,arr[i]+arr[j]+arr[k])
+print(max_sum)
