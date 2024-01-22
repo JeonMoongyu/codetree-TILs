e419f7e46ad8b14bb20d952dbc4f45c4dd5af7c8
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-22 22:28:47 +0900

    Title: Carry 피하기 2, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 240122/Carry 피하기 2/escaping-carry-2.py
+++ 240122/Carry 피하기 2/escaping-carry-2.py
@@ -11,8 +11,8 @@ arr = [
 ]
 max_sum = -1
 for i in range(n-2):
-    for j in range(n-1):
-        for k in range(n):
+    for j in range(i+1,n-1):
+        for k in range(j+1,n):
             if not_carry(arr[i],arr[j],arr[k]):
                 max_sum = max(max_sum,arr[i]+arr[j]+arr[k])
 print(max_sum)
