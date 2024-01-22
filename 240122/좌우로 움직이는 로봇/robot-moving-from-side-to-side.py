d4952ee060b8a0d9dfcb4edf557848b839bc5d85
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-22 11:19:30 +0900

    Title: 좌우로 움직이는 로봇, Time: 399ms, Memory: 63MB, Status: Passed - CodeTree

--- 240122/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240122/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -9,15 +9,22 @@ for _ in range(n):
         a.append(a[-1]+d)       
 
 b = [0]
-for _ in range(n):
+for _ in range(m):
     t, d = input().split()
     t = int(t)
     d = 1 if d == 'R' else -1
     for __ in range(t):
         b.append(b[-1]+d) 
 
+if len(a) > len(b):
+    length = len(a)
+    b += [ b[-1] for _ in range(len(a)-len(b)) ]
+elif len(a) < len(b):
+    length = len(b)
+    a += [ a[-1] for _ in range(len(b)-len(a)) ]
+
 ans = 0
-for i in range(2,len(a)):
+for i in range(1,length):
     if a[i-1] != b[i-1] and a[i] == b[i]:
         ans += 1
 print(ans)
