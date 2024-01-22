55ac807a9232742efe7b052bfbab8ac2671330c6
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-22 15:31:31 +0900

    Title: 격자 위의 편안한 상태, Time: 302ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240122/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -0,0 +1,30 @@
+n,m = tuple(map(int,input().split()))
+grid = [
+    [0 for _ in range(n+1)]
+    for _ in range(n+1)
+]
+
+
+def in_range(i,j):
+    return 1<=i and i<=n and 1<=j and j<=n
+
+
+dis, djs = [0,0,1,-1], [1,-1,0,0]
+
+
+def is_comfortable(i,j):
+    cnt = 0
+    for d in range(4):
+        ni, nj = i+dis[d], j+djs[d] 
+        if in_range(ni,nj):
+            cnt += grid[ni][nj]
+    return cnt >= 3
+    
+
+for _ in range(m):
+    r, c = tuple(map(int,input().split()))
+    grid[r][c] = 1
+    if is_comfortable(r,c):
+        print(1)
+    else:
+        print(0)
