92b68d52164e8f50799e866e5abf180c53bd6793
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-22 21:34:13 +0900

    Title: 체크판위에서 2, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 240122/체크판위에서 2/on-the-checkboard-2.py
+++ 240122/체크판위에서 2/on-the-checkboard-2.py
@@ -4,10 +4,11 @@ board = [
     for _ in range(R)
 ]
 ans = 0
-for i in range(1,R-1):
-    for j in range(1,C-2):
-        for k in range(i+1,R-1):
-            for l in range(j+1,C-1):
-                if board[i][j] != board[0][0] and board[k][l] == board[0][0]:
-                    ans += 1
+if board[0][0] != board[R-1][C-1]:
+    for i in range(1,R-1):
+        for j in range(1,C-2):
+            for k in range(i+1,R-1):
+                for l in range(j+1,C-1):
+                    if board[i][j] != board[0][0] and board[k][l] == board[0][0]:
+                        ans += 1
 print(ans)
