ff6e379a44b7f11a36652215b5ea1d1b8d4c1138
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-24 16:03:22 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 233ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240124/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -0,0 +1,11 @@
+n = int(input())
+a,b,c = tuple(map(int,input().split()))
+
+ans = 0
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        for k in range(1,n+1):
+            if abs(i-a)<=2 or abs(j-b)<=2 or abs(k-c)<=2:
+                ans += 1
+
+print(ans)
