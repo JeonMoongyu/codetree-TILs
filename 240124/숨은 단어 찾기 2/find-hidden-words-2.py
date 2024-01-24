70a17a4a309b1d00ca07a5e7d4efed7e0311164d
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-24 13:45:15 +0900

    Title: 숨은 단어 찾기 2, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240124/숨은 단어 찾기 2/find-hidden-words-2.py
+++ 240124/숨은 단어 찾기 2/find-hidden-words-2.py
@@ -14,10 +14,11 @@ dis, djs = [0,-1,-1,-1,0,1,1,1], [1,1,0,-1,-1,-1,0,1]
 ans = 0
 for i in range(n):
     for j in range(m):
-        if arr[i][j] == 'L':
-            for di,dj in zip(dis,djs):
-                if in_range(i+di,j+dj) and in_range(i+di*2,j+dj*2) and\
-                   arr[i+di][j+dj]=='E' and arr[i+di*2][j+dj*2]=='E':
-                   ans += 1
+        if arr[i][j] != 'L':
+            continue
+        for di,dj in zip(dis,djs):
+            if in_range(i+di,j+dj) and in_range(i+di*2,j+dj*2) and\
+               arr[i+di][j+dj]=='E' and arr[i+di*2][j+dj*2]=='E':
+               ans += 1
 
 print(ans)
