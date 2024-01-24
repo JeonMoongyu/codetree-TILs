d3680ba37bc8d1e65c3692bf47b989a981de6c4c
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-24 15:25:24 +0900

    Title: 바구니 안의 사탕 2, Time: 163ms, Memory: 25MB, Status: Passed - CodeTree

--- 240124/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240124/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -4,11 +4,17 @@ for _ in range(n):
     candies, box = tuple(map(int,input().split()))
     arr[box] += candies
 
+
+def in_range(i):
+    return 0<=i and i<101
+
+    
 ans = 0
-for c in range(k,101-k):
+for c in range(101):
     num_of_candies = 0
     for i in range(c-k,c+k+1):
-        num_of_candies += arr[i]
+        if in_range(i):
+            num_of_candies += arr[i]
     ans = max(ans,num_of_candies)
 
 print(ans)
