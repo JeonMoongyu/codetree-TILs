cefdc25b41be81d6312a947967e653c1a483eb9b
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-24 18:55:04 +0900

    Title: 상해버린 치즈, Time: 187ms, Memory: 25MB, Status: Passed - CodeTree

--- 240124/상해버린 치즈/rotten-cheese.py
+++ 240124/상해버린 치즈/rotten-cheese.py
@@ -8,15 +8,18 @@ sick = [
     for _ in range(s)
 ]
 
-rotted = [0 for _ in range(m+1)]
-for sick_person, sick_time in sick:
+rotted = [
+    [0 for _ in range(s)]
+    for _ in range(m+1)
+]
+for j, (sick_person, sick_time) in enumerate(sick):
     for eat_person, cheese, eat_time in data:
         if sick_person == eat_person and eat_time < sick_time:
-            rotted[cheese] += 1
+            rotted[cheese][j] = 1
 
 possible_sick = [0 for _ in range(n+1)]
 for eat_person, cheese, eat_time in data:
-    if rotted[cheese] == s:
+    if sum(rotted[cheese]) == s:
         possible_sick[eat_person] = 1
 
 ans = 0
