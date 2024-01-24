7ffef60672e26b76ff75aba3faccebb039553f72
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-25 08:54:38 +0900

    Title: 이상한 폭탄 2, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

--- 240125/이상한 폭탄 2/strange-bomb-2.py
+++ 240125/이상한 폭탄 2/strange-bomb-2.py
@@ -4,11 +4,15 @@ bombs = [
     for _ in range(n)
 ]
 
+
+def in_range(i):
+    return 0<=i and i<n
+
 ans = -1
-for i in range(n-k):
+for i in range(n):
     exists = False
     for j in range(i+1,i+k+1):
-        if bombs[j] == bombs[i]:
+        if in_range(j) and bombs[j] == bombs[i]:
             exists = True
             break
     if exists:
