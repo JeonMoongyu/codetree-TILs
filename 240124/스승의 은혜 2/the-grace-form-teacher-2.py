50db7e7aab11a675f6ad073b9e8d7760560d6ee4
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-24 18:19:12 +0900

    Title: 스승의 은혜 2, Time: 226ms, Memory: 25MB, Status: Passed - CodeTree

--- 240124/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240124/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -11,9 +11,12 @@ for i in range(n):
     prices[i] *= 2
 
     sum_prices = 0
+    cnt = 0
     for j in range(n):
         sum_prices += sorted_prices[j]
-        if sum_prices <= budget:
-            ans = max(ans,j+1)
-        
+        if sum_prices > budget:
+            break
+        cnt += 1
+    ans = max(ans,cnt)
+
 print(ans)
