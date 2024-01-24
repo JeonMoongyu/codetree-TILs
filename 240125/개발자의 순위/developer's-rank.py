a0634e65d369ccd6f738ecdf67aaa20c4ffb1424
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-25 08:45:56 +0900

    Title: 개발자의 순위, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240125/개발자의 순위/developer's-rank.py
+++ 240125/개발자의 순위/developer's-rank.py
@@ -1,14 +1,8 @@
 k,n = tuple(map(int,input().split()))
-data1 = [
+data = [
     list(map(int,input().split()))
     for _ in range(k)
 ]
-data2 = [
-    [ (data1[i][j],j+1) for j in range(n)]
-    for i in range(k)
-]
-for i in range(k):
-    data2[i].sort(key = lambda x: x[0])
 
 ans = 0
 for a in range(1,n+1):
@@ -16,8 +10,10 @@ for a in range(1,n+1):
         if a == b:
             continue
         satisfied = True
-        for a_match in data2:
-            if a_match[a-1][1] > a_match[b-1][1]:
+        for arr in data:
+            rank_a = arr.index(a) # +1
+            rank_b = arr.index(b) # +1
+            if rank_a > rank_b:
                 satisfied = False
         if satisfied:
             ans += 1
