5bfd1716200c79d9b79c6142213fb5dc08505f99
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-25 17:06:11 +0900

    Title: 등차수열, Time: 134ms, Memory: 25MB, Status: Passed - CodeTree

--- 240125/등차수열/arithmetic-sequence.py
+++ 240125/등차수열/arithmetic-sequence.py
@@ -3,9 +3,11 @@ seq = list(map(int,input().split()))
 
 ans = 0
 for k in range(1,101):
+    cnt = 0
     for i in range(n):
         for j in range(i+1,n):
             if 2*k == seq[i] + seq[j]:
-                ans += 1
+                cnt += 1
+    ans = max(ans,cnt)
 
 print(ans)
