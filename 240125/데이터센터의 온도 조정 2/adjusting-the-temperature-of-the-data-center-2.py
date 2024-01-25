c417b4c85344700af4ee9b87377436953a663309
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-25 09:45:18 +0900

    Title: 데이터센터의 온도 조정 2, Time: 146ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240125/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -0,0 +1,20 @@
+n,c,g,h = tuple(map(int,input().split()))
+temp = [
+    tuple(map(int,input().split()))
+    for _ in range(n)
+]
+
+min_temp = sorted(temp, key= lambda x: x[0])[0][0]
+max_temp = sorted(temp, key= lambda x: x[1])[n-1][1]
+left_end = max(0,min_temp-1)
+right_end = max_temp
+
+work = [0] * (right_end+1)
+for ta,tb in temp:
+    for t in range(left_end,ta):
+        work[t] += c
+    for t in range(ta,tb):
+        work[t] += g
+    for t in range(tb,right_end+1):
+        work[t] += h
+print(max(work))
