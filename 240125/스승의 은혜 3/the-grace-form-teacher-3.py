8090b373cf9e47ce36c898ca751014e4272bf0b1
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-25 09:15:58 +0900

    Title: 스승의 은혜 3, Time: 586ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240125/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,22 @@
+n,budget = tuple(map(int,input().split()))
+arr = [
+    tuple(map(int,input().split()))
+    for _ in range(n)
+]
+
+ans = 0
+for i in range(n):
+    arr[i] = (arr[i][0]//2, arr[i][1])
+    arr_ith = [ arr[k] for k in range(n) ]
+    arr[i] = (arr[i][0]*2, arr[i][1]) 
+    arr_ith.sort(key= lambda x: x[0]+x[1])
+
+    cost, num_of_students = 0, 0
+    for j in range(n):
+        if cost + arr_ith[j][0] + arr_ith[j][1] > budget:
+            break
+        cost += arr_ith[j][0] + arr_ith[j][1]
+        num_of_students += 1
+    ans = max(ans,num_of_students)
+
+print(ans)
