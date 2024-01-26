96e33a4e404c2ae4e66942c22f99edf53a2bb188
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-26 17:37:06 +0900

    Title: 팀으로 하는 틱택토 2, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240126/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 240126/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -4,28 +4,46 @@ arr = [
 ]
 
 
-def cnt(_list):
+def did_won(_list):
     exist = [0] * 10
     for elem in _list:
         exist[elem] = 1
-    return sum(exist)
+    return sum(exist)==2
 
 
-ans = 0
+def who_won(_list):
+    a = _list[0]
+    for elem in _list[1:]:
+        if elem != a:
+            return min(a,elem),max(a,elem)
+
+
+won = [
+    [0 for _ in range(10)]
+    for _ in range(10)
+]
+
 for i in range(3):
-    if cnt(arr[i]) == 2:
-        ans += 1
+    if did_won(arr[i]):
+        a,b = who_won(arr[i])
+        won[a][b] = 1
 for j in range(3):
     col_j = [ arr[i][j] for i in range(3) ]
-    if cnt(col_j) == 2:
-        ans += 1
+    if did_won(col_j):
+        a,b = who_won(col_j)
+        won[a][b] = 1
 
 diag1 = [ arr[i][i] for i in range(3) ]
-if cnt(diag1) == 2:
-    ans += 1
+if did_won(diag1):
+    a,b = who_won(diag1)
+    won[a][b] = 1
 
 diag2 = [ arr[i][2-i] for i in range(3) ]
-if cnt(diag2) == 2:
-    ans += 1
+if did_won(diag2):
+    a,b = who_won(diag2)
+    won[a][b] = 1
 
+ans = 0
+for row in won:
+    ans += sum(row)
 print(ans)
