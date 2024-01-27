4700f38caf5f87f7afcf5c7450606cade55925b8
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-27 23:43:13 +0900

    Title: 훌륭한 점프, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240127/훌륭한 점프/great-jump.py
+++ 240127/훌륭한 점프/great-jump.py
@@ -1,12 +1,16 @@
 n,k = tuple(map(int,input().split()))
 stones = list(map(int,input().split()))
 
+
 def max_dist(arr, max_val):
     indices = [ i for i in range(len(arr)) if arr[i] <= max_val ]
     distances = [ indices[j+1]-indices[j] for j in range(len(indices)-1) ]
     return max(distances)
 
-for a in range(n,0,-1):
-    if max_dist(stones,a) > k:
-        print(a+1)
+
+ans = n
+while ans > 1:
+    if max_dist(stones, ans-1) > k:
         break
+    ans -= 1
+print(ans)
