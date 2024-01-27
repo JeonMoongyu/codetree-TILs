e262fc61ceb07ee8f2e0cf60d0fbc41eb8851d6c
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-27 23:47:21 +0900

    Title: 훌륭한 점프, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240127/훌륭한 점프/great-jump.py
+++ 240127/훌륭한 점프/great-jump.py
@@ -2,15 +2,18 @@ n,k = tuple(map(int,input().split()))
 stones = list(map(int,input().split()))
 
 
+def end_points_ok(arr, max_val):
+    return arr[0] <= max_val and arr[1] <= max_val
+
 def max_dist(arr, max_val):
-    indices = [ i for i in range(len(arr)) if arr[i] <= max_val ]
+    indices = [ i for i in range(1, len(arr)1-1) if arr[i] <= max_val ]
     distances = [ indices[j+1]-indices[j] for j in range(len(indices)-1) ]
     return max(distances)
 
 
 ans = n
 while ans > 1:
-    if max_dist(stones, ans-1) > k:
+    if not end_points_ok(stones, ans-1) or max_dist(stones, ans-1) > k:
         break
     ans -= 1
 print(ans)
