e9de25df3bb78be2a4d31c37059df00b3281e891
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-27 23:48:49 +0900

    Title: 훌륭한 점프, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240127/훌륭한 점프/great-jump.py
+++ 240127/훌륭한 점프/great-jump.py
@@ -6,7 +6,7 @@ def end_points_ok(arr, max_val):
     return arr[0] <= max_val and arr[-1] <= max_val
 
 def max_dist(arr, max_val):
-    indices = [ i for i in range(1, len(arr)1-1) if arr[i] <= max_val ]
+    indices = [ i for i in range(len(arr)) if arr[i] <= max_val ]
     distances = [ indices[j+1]-indices[j] for j in range(len(indices)-1) ]
     return max(distances)
 
