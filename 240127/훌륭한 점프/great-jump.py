3a5ec2431e2e1545746630ffc7ca57b672f83b3b
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-27 23:55:13 +0900

    Title: 훌륭한 점프, Time: 154ms, Memory: 24MB, Status: Passed - CodeTree

--- 240127/훌륭한 점프/great-jump.py
+++ 240127/훌륭한 점프/great-jump.py
@@ -9,10 +9,8 @@ def max_dist(arr, max_val):
 
 
 ans = max(stones[0], stones[-1])
-while ans < 100:
+while ans <= 100:
     if max_dist(stones, ans) <= k:
         print(ans)
         break
     ans += 1
-if ans == 100:
-    print(100)
