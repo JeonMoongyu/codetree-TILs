f0b3a00dcdddcdd3ee95113c2263ce0ad89dcc38
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-28 02:47:11 +0900

    Title: 초기 수열 복원하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240128/초기 수열 복원하기/restore-initial-sequence.py
+++ 240128/초기 수열 복원하기/restore-initial-sequence.py
@@ -2,16 +2,16 @@ n = int(input())
 arr = list(map(int,input().split()))
 
 
-def list_of_perm(n):
-    if n == 1:
+def list_of_perm(m):
+    if m == 1:
         return [[1]]
-    return sorted([ ex_perm[:i] + [n] + ex_perm[i:] \
-                    for i in range(n) \
-                    for ex_perm in list_of_perm(n-1) ])
+    return sorted([ ex_perm[:i] + [m] + ex_perm[i:] \
+                    for i in range(m) \
+                    for ex_perm in list_of_perm(m-1) ])
 
 
-def adj_sum(perm):
-    return [ perm[i]+perm[i+1] for i in range(n-2) ]
+def adj_sum(seq):
+    return [ seq[i]+seq[i+1] for i in range(n-2) ]
 
 
 for perm in list_of_seq(n):
