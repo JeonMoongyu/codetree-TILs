d728341e4832f3b328a87dc2c377efd3f06653af
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-31 00:43:25 +0900

    Title: 구역 청소, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240131/구역 청소/district-cleaning.py
+++ 240131/구역 청소/district-cleaning.py
@@ -1,9 +1,7 @@
 a, b = tuple(map(int,input().split()))
 c, d = tuple(map(int,input().split()))
-arr = [0] * 100
 
-for i in range(100):
-    if a<=i and i<b or c<=i and i<d:
-        arr[i] = 1
-
-print(sum(arr))
+if d < a or b < c:
+    print(b-a+d-c)
+else:
+    print(max(b,d)-min(a,c))
