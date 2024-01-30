d25f8e7644bfa3cdf0ca6ffff23b0007b9305d3d
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-31 02:49:59 +0900

    Title: X 달리기, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240131/X 달리기/x-run.py
@@ -0,0 +1,14 @@
+x = int(input())
+
+
+def max_dist(t):
+    if t % 2 == 0:
+        return (2*t+1)*t//4
+    else:
+        return (t+1)**2//4
+
+
+for t in range(1,200):
+    if max_dist(t) >= x:
+        print(t)
+        break
