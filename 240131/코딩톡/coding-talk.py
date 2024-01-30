ff76815d6dceba8ebc79afcf9b90b50ad2f4e9d4
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-01-31 02:29:50 +0900

    Title: 코딩톡, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240131/코딩톡/coding-talk.py
@@ -0,0 +1,27 @@
+n, m, p = tuple(map(int,input().split()))
+data = [0]
+for _ in range(m):
+    c, u = input().split()
+    data.append((c,int(u)))
+
+check = [1] * n
+
+
+def alp_to_num(x):
+    return ord(x)-ord('A')
+
+
+def num_to_alp(k):
+    return chr(k+ord('A'))
+
+
+for c, u in data[p:]:
+    check[alp_to_num(c)] = 0
+
+for c, u in data[1:p]:
+    if u == data[p][1]:
+        check[alp_to_num(c)] = 0
+
+for i in range(n):
+    if check[i]:
+        print(num_to_alp(i), end=" ")
