890bc0eb046b5ab059fd6d402af0a2fa5178563c
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-02-06 06:41:58 +0900

    Title: 삽입 정렬 구현, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240206/삽입 정렬 구현/implement-insertion-sort.py
+++ 240206/삽입 정렬 구현/implement-insertion-sort.py
@@ -5,15 +5,11 @@ arr = list(map(int,input().split()))
 def insertion_sort():
     for i in range(1,n):
         key = arr[i]
-        for j in range(i-1,-1,-1):
-            if arr[j] > key:
-                arr[j+1] = arr[j]
-            else:
-                break
-        if j == 0 and arr[0] > key:
-            arr[0] = key
-        else:
-            arr[j+1] = key
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
             
 
 
