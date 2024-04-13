895fc81c585267a9bd63fe7882ea3adffed96405
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-04-13 16:56:00 +0900

    Title: 소수점 반올림하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240413/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    double a = 25.352;
+    cout << fixed;
+    cout.precision(1);
+    cout << a;
+
+    return 0;
+}
