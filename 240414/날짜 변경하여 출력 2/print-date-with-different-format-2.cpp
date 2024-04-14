f5bd03ba8829e2796e47eb1e48350050f3239093
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-04-14 18:21:31 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240414/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int y, m, d;
+    char x;
+    cin >> m >> x >> d >> x >> y;
+    cout << y << "." << m << "." << d;
+    return 0;
+}
