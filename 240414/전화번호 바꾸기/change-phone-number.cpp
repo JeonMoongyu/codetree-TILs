d22e556744ac0eb9bd6272bdb79e502a7dccd1fe
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-04-14 18:24:18 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240414/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a, b, x;
+    char c;
+    cin >> x >> c >> a >> c >> b;
+    cout << "010-" << b << "-" << a;
+    return 0;
+}
