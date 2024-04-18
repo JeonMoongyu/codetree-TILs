afd9b0e6f581949696acbe99c46b2f0e9e33ab3e
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-04-19 00:16:03 +0900

    Title: 짝수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240419/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int b, a;
+    cin >> b >> a;
+    int i = b;
+    while ( i >= a ){
+        cout << i << " ";
+        i -= 2;
+    }
+    return 0;
+}
