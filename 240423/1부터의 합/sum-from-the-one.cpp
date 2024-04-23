fae68d355b16bb27396f7b50e7856ae8d6de3233
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-04-23 18:41:45 +0900

    Title: 1부터의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240423/1부터의 합/sum-from-the-one.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum_val=0;
+    cin >> n;
+    for (int i=1; i<=n; i++){
+        sum_val += i;
+        if (sum_val>=n){
+            cout << i;
+            break;
+        }
+    }
+    return 0;
+}
