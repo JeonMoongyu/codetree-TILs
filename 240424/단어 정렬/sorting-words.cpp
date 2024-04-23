bad3cbc76814fcdffd784cecee90c1fdbe6a3f49
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-04-24 01:36:23 +0900

    Title: 단어 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240424/단어 정렬/sorting-words.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string arr[100];
+    for (int i=0; i<n; i++)
+        cin >> arr[i];
+    sort(arr, arr+n);
+    for (int i=0; i<n; i++)
+        cout << arr[i] << endl;
+    return 0;
+}
